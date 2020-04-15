17833ebb6236c95da540c8964b6365729a68e8a8
waiphyo <waiphyoko@myanmarsp.com>
2020-04-15 15:55:54 +0630

    weather controller added

---
+++ src/Core3Api/Controllers/WeatherController.cs
@@ -0,0 +1,21 @@
+using Core3Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core3Api.Controllers
+{    
+    public class WeatherController : ControllerBase
+    {       
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
