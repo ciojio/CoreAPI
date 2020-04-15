7b44293fa32ad4333ebc2cbb4a0163b837679512
waiphyo <waiphyoko@myanmarsp.com>
2020-04-15 16:10:22 +0630

    weather controller added

--- src/Core3Api/Controllers/WeatherController.cs
+++ src/Core3Api/Controllers/WeatherController.cs
@@ -1,10 +1,18 @@
 using Core3Api.Services;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Core3Api.Controllers
 {    
     public class WeatherController : ControllerBase
     {       
+        private static readonly string Summaries = new[]
+        {
+            "Freezing","Bracing","Chilly","Cool","Mild", "Warm", "Balmy","Hot","Sweltering","Scorching"
+        };
+
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
