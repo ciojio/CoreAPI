293d72baf28d4251ed02f6cd96a1be55e615bd55
waiphyo <waiphyoko@myanmarsp.com>
2020-04-28 12:09:49 +0630

    test

--- src/Core3Api/Controllers/HomeController.cs
+++ src/Core3Api/Controllers/HomeController.cs
@@ -20,23 +20,32 @@ namespace Core3Api.Controllers
             string emailSvr = Environment.GetEnvironmentVariable("EmailServer"); 
             string connectionString = Environment.GetEnvironmentVariable("ConnectionString");
             string name = "";
+            string error_message = "";
             //connectionString = "User Id = scott; Password = 12345; Data Source = localhost:1521/orcl;";
 
-            /*
-            using (OracleConnection connection = new OracleConnection(connectionString))
+            try
             {
-                string queryString = "Select * from emp";
-                OracleCommand command = new OracleCommand(queryString, connection);
-                command.Connection.Open();
-                //command.ExecuteNonQuery();
-                OracleDataReader reader = command.ExecuteReader();
-                while (reader.Read())
+                using (OracleConnection connection = new OracleConnection(connectionString))
                 {
-                    name = reader.GetString(1);
+                    string queryString = "Select * from emp";
+                    OracleCommand command = new OracleCommand(queryString, connection);
+                    command.Connection.Open();
+                    //command.ExecuteNonQuery();
+                    OracleDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        name = reader.GetString(1);
+                        break;
+                    }
                 }
-            }*/
+            }
+            catch(Exception ex)
+            {
+                error_message = ex.Message.ToString() + " #### " + ex.InnerException.Message.ToString();
+            }
+            
 
-            return Ok("hello world "+ name +" " + emailSvr+" "+connectionString);
+            return Ok("hello world "+ name +" " + emailSvr+" "+connectionString+ " ### " + error_message);
 
 			//string OC_Evariable = Environment.GetEnvironmentVariable("OC_HC_URL");
             //return Ok("hello world " + emailSvr+" "+connectionString+" "+OC_Evariable);
